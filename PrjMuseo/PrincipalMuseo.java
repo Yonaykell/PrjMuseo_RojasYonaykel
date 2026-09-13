0b8470ec9f1e1c4c45abe6cba6bd6304d158acfa
Yonaykel <yonaykel.rivas2008@gmail.com>
2026-09-13 16:39:18 -0600

    feat: PrincipalMuseo, sin errores de compilacion

--- PrjMuseo/PrincipalMuseo.java
+++ PrjMuseo/PrincipalMuseo.java
@@ -1,33 +1,25 @@
-
-/**
- * Write a description of class PrincipalMuseo here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
-public class PrincipalMuseo
-{
-    // instance variables - replace the example below with your own
-    private int x;
-
-    /**
-     * Constructor for objects of class PrincipalMuseo
-     */
-    public PrincipalMuseo()
-    {
-        // initialise instance variables
-        x = 0;
-    }
-
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+public class PrincipalMuseo {
+    public static void main(String[] args) {
+        Persona a, b, c;
+        a = new Persona("Nicolás Maduro", "666-6");
+        b = new Persona("Donald Trump", "333-3");
+        c = new Persona("Claudia Sheinbaum", "777-7");
+        BoletoMuseo b1, b2, b3;
+        b1 = new BoletoMuseo(4500.0);
+        b2 = new BoletoMuseo(6000.0);
+        b3 = new BoletoMuseo(5800.0);
+        VentaDelDia vd; 
+        vd = new VentaDelDia();
+        a.asignarBoleto(b1);
+        System.out.println("Detalle del primer objeto Persona: " + a.toString());
+        vd.registrarVentaBoleto(b1);
+        b.asignarBoleto(b2);
+        System.out.println("Detalle del segundo objeto Persona: " + b.toString());
+        vd.registrarVentaBoleto(b2);
+        c.asignarBoleto(b3);
+        System.out.println("Detalle del tercer objeto Persona: " + c.toString());
+        vd.registrarVentaBoleto(b3);
+        System.out.println("Contador global de boletos creados: " + BoletoMuseo.getContador());
+        System.out.println("Detalle de la Venta Del Día: " + vd.toString());
     }
 }
